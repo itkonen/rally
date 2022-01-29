dfa4a293406015ce6a8f9a1fd010d0764b1f5c8b
Juha Itkonen <itkonen.juha@gmail.com>
2022-01-29 09:58:15 +0200

    title

--- R/plot.R
+++ R/plot.R
@@ -5,7 +5,7 @@ plot_history <- function(con = sqlite_connection()) {
   plotly::plot_ly(y, x = ~time, y = ~temp_current, color = ~name,
                   type = "scatter", mode = "line") %>%
     plotly::layout(hovermode = "x unified",
-                   title = list(text = "Temperature"),
+                   title = list(text = "Room Temperature"),
                    xaxis = list(title = NA),
                    yaxis = list(title = NA, fixedrange = TRUE)) %>%
       plotly::config(modeBarButtonsToRemove = c("zoomIn2d", "zoomOut2d"))
